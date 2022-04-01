08436895c6edb2d46af662269772d5b808460a44
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-03-31 20:16:37 -0400

    Impleted Dynamic Tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,33 +4,62 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+// Fake data taken from initial-tweets.json
+const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
 const createTweetElement = function(tweetData) {
-  $dataCreated = new Date(tweetData.created_at);
-  $dateToday = new Date();
-  //create the structure of tweet to be included in the html
-  const $tweet = (`<article class="tweet">Hello world</article>`);
-
-
+  //create the structure of tweet to be included in the html (using previous html article)
+const $tweet = (`
+<article class="tweet-component">
+  <div class="image-username-refkey">
+    <div class="image-username">
+      <img src="${tweetData.user.avatars}" alt=""> 
+      <span>${tweetData.user.name}</span>
+    </div>
+    <div>
+      ${tweetData.user.handle}
+    </div>
+  </div>
+  <div class="tweet-content">
+    <p>${tweetData.content.text}</p>
+  </div>
+  <div class="time-reactions">
+    <p>${new Date(tweetData.created_at)}</p>
+    <div class="icons">
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+    </div>
+  </div>
+</article>`
+);
   //return the structure to append to html
   return $tweet;
 }
 
-
-const $tweet = createTweetElement(tweetData);
-// to add it to the page so we can make sure it's got all the right elements, classes, etc.
-$('#tweets-container').append($tweet); 
 
 
 const renderTweets = (tweets) => {
@@ -39,13 +68,17 @@ const renderTweets = (tweets) => {
   $("#tweets-container").empty();
 
   // loops through tweets from newer to older
-  for (let i in tweets) {
+  for (let tweet of tweets) {
 
     // calls createTweetElement for each tweet
-    $tweet = createTweetElement(tweets[i]);
+    const $tweet = createTweetElement(tweet);
 
     // takes return value and appends it to the tweets container
     $('#tweets-container').prepend($tweet);
   }
 
 }
+
+$(document).ready( () => {
+  renderTweets(data);
+});
